6826e4193a1515dbb35e3bee7a43847613127200
Namit Nagar <89021639+NamitNagar@users.noreply.github.com>
2022-10-05 16:48:06 +0530

    Update chatapp.js

---
+++ chatapp.js
@@ -0,0 +1,6 @@
+var recipentContainer = document.getElementById("recipent-container");
+var chatContainer = document.getElementById("chat-container");
+function openChat(){
+    recipentContainer.style.width = "20px";
+    chatContainer.style.display = "flex";
+}
